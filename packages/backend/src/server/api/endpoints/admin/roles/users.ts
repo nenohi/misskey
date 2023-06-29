a57df6973070822edb7eb804df28a5c45bf5fd08
riku6460 <17585784+riku6460@users.noreply.github.com>
2023-06-30 06:10:55 +0900

    Update packages/backend/src/server/api/endpoints/admin/roles/users.ts

--- packages/backend/src/server/api/endpoints/admin/roles/users.ts
+++ packages/backend/src/server/api/endpoints/admin/roles/users.ts
@@ -63,11 +63,11 @@ export default class extends Endpoint<typeof meta, typeof paramDef> {
 					.orWhere('assign.expiresAt > :now', { now: new Date() });
 				}))
 				.innerJoinAndSelect('assign.user', 'user');
-			
+
 			if (ps.userId != null) {
 				query.andWhere('assign.userId = :userId', { userId: ps.userId });
 			}
-			
+
 			const assigns = await query
 				.take(ps.limit)
 				.getMany();
